614de6725c887089023b8093affdf6e00e8a1bdf
Upal Barua <upalbarua52@gmail.com>
2022-11-04 08:25:17 +0600

    Added basic routing setup

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,26 +1,9 @@
 import React from 'react';
-import Button from '../UI/Button/Button';
-import Container from '../UI/Container/Container';
+import { RouterProvider } from 'react-router-dom';
+import router from '../../routes/Routes';
 
 function App() {
-  return (
-    <Container>
-      <h2 className="primary-title">About Us</h2>
-      <p className="secondary-title">
-        We are qualified & of experience in this field
-      </p>
-      <p className="body-text">
-        There are many variations of passages of Lorem Ipsum available, but the
-        majority have suffered alteration in some form, by injected humour, or
-        randomised words which don't look even slightly believable.{' '}
-      </p>
-      <p className="body-text">
-        the majority have suffered alteration in some form, by injected humour,
-        or randomised words which don't look even slightly believable.{' '}
-      </p>
-      <Button variant={'secondary'}>Appointment</Button>
-    </Container>
-  );
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, itaque?
+      </p>
+    </nav>
+  );
+};
+
+export default Navbar;
